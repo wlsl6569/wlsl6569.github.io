59b952e3f1f9c4e67385dc13c0b78b037096166d
JINHEE JEON <71010429+wlsl6569@users.noreply.github.com>
2023-04-09 09:35:39 +0900

    chatbot

---
+++ response.js
@@ -0,0 +1,16 @@
+const responseObj = {
+
+
+
+    hello: "Hey ! How are you doing ?",
+    hey: "Hey! What's Up",
+    today: new Date().toDateString(),
+    time: new Date().toLocaleTimeString(),
+    안녕: "안녕하세요?",
+    자기소개: "제 이름은 전진희이고 코딩을 배우고 있어요.",
+    포트폴리오: "이 사이트가 제 포트폴리오에요.",
+    기술: "저는 HMTL, CSS, JavaScript를 알고있고 앞으로 Python과 AI에 대해서도 배우게 될 거에요."
+
+  };
+
+  
